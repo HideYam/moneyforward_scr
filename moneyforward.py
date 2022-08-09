b55ab949dbf6f40e46f179e6a198decae230177e
HideYam <hide.yamauchi@gmail.com>
2022-08-09 09:28:53 +0900

    Add files via upload

---
+++ moneyforward.py
@@ -0,0 +1,93 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import datetime
+from time import sleep
+import matplotlib.pyplot as plt
+import chromedriver_binary
+from dotenv import load_dotenv
+
+# .envファイルの内容を読み込む
+load_dotenv()
+
+option = Options()                          # オプションを用意
+option.add_argument('--incognito')          # シークレットモードの設定を付与
+
+# seleniumでのダウンロード先のフォルダの設定(※必ずフルパス,env読み込み)
+
+option.add_experimental_option("prefs", {"download.default_directory": download_path})
+
+browser = webdriver.Chrome(options=option)   # Chromeを準備(optionでシークレットモードにしている）
+
+
+### login
+url = "https://id.moneyforward.com/sign_in/email"
+user_name = USER
+password = PASS
+browser.get(url)
+
+#メールアドレスを入力
+e = browser.find_element(By.NAME,"mfid_user[email]")
+e.clear()
+e.send_keys(user_name)
+
+#　ログインボタンを押す
+frm = browser.find_element(By.XPATH,"//form[1]")
+frm.submit()
+
+sleep(3)
+
+#　パスワードを入力
+e = browser.find_element(By.NAME,"mfid_user[password]")
+e.clear()
+e.send_keys(password)
+
+#　ログインボタンを押す
+frm = browser.find_element(By.XPATH,"//form[1]")
+frm.submit()
+
+sleep(3)
+
+link = browser.find_element(By.CLASS_NAME,"serviceItem")
+link.click()
+
+sleep(3)
+
+frm = browser.find_element(By.XPATH,"//form[1]")
+frm.submit()
+
+sleep(10)
+
+#　家計簿アクセスしてダウンロード
+link = browser.find_element(By.LINK_TEXT, "家計")
+link.click()
+
+sleep(5)
+
+def pagelink():
+    page_link = browser.find_element(By.CLASS_NAME, "fc-button-content")
+    page_link.click()
+
+def csvdl():
+    dl_link = browser.find_element(By.CLASS_NAME, "icon-download-alt")
+    dl_link.click()
+    sleep(1)
+    file_link = browser.find_element(By.LINK_TEXT, "CSVファイル")
+    file_link.click()
+
+
+
+# p1
+csvdl()
+sleep(2)
+
+#p2 -
+for i in range(2):
+    pagelink()
+    sleep(5)
+    csvdl()
+    sleep(2)
+
+# ブラウザを終了
+browser.close()
---
+++ moneyforward_csv.py
@@ -0,0 +1,39 @@
+# import module
+import pandas as pd
+import glob
+import os
+import openpyxl
+from dotenv import load_dotenv
+
+# .envファイルの内容を読み込む
+load_dotenv()
+
+download_path = '/Users/hideki/Documents/GitHub/moneyforward_scr/tmp'
+
+
+# ディレクトリ変更
+os.chdir(download_path)
+
+# 空のDataFrameを定義
+df = pd.DataFrame()
+
+# .csvを含むファイルをpd.read_csv()で読み込む
+firstLoop = True
+for i in glob.glob("*.csv*"):
+    # header=0 ヘッダーあり
+    tmp_df = pd.read_csv(i, encoding="shift-jis")
+    #ヘッダー行除外 1回目だけはヘッダーあり、２回目以降は削除
+    print(i)
+    #if firstLoop:
+    #    firstLoop = False
+        #df.columns=tmp_df[:0]
+    #else:
+    #    tmp_df = tmp_df[1:]
+
+    # DataFrameを連結する
+    df = pd.concat([df, tmp_df])
+
+#EXCEL
+
+print(df)
+df.to_excel(download_path + '/output.xlsx', sheet_name='mfdata', index=False, header=True)
